04613249fae56d098b9b38a05661f27a676bc32d
VanikSarajyan <vaniksarajyan@gmail.com>
2022-10-07 12:10:28 +0400

    py hw5 1

---
+++ python_homeworks/homework_5/time_machine.py
@@ -0,0 +1,16 @@
+from hashlib import new
+
+
+hour = int(input("Enter an hour: "))
+m = int(input("am(1) or pm(2)?: "))
+ahead = int(input("How many hours ahead?: "))
+
+if hour <= 12 and (m == 1 or m == 2):
+    m -= 1
+    new_hour = (hour + ahead) % 12
+    m += ((hour + ahead) // 12 ) % 2
+    m %= 2
+    period = "am" if m == 0 else "pm"
+    print(f"New hour: {new_hour} {period}")
+else:
+    print("Invalid input.")
